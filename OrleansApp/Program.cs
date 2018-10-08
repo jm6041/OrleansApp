d7def52f40e38ccbcf2d00f657c85613274bcbef
JM <jmmail@foxmail.com>
2018-10-08 10:15:22 +0800

    添加项目文件。

---
+++ OrleansApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrleansApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
